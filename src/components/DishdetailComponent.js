fd45e0bbd494838f0784321478178c32104bb008
hidai25 <hidai25@gmail.com>
2018-12-29 14:56:49 +0200

    task 3 + begining of week2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,77 +1,69 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+class DishDetail extends Component {
 
-class Dishdetail extends Component {
-
-  renderComments(comments) {
-          if (comments == null) {
-              return (<div></div>)
-          }
-
-          const cmnts=comments.map(comment=>{
+    renderDish(dish) {
+        if (dish != null)
             return (
-              <li key={comment.id}>
-              <p>{comment.comment}</p>
-              <p>-- {comment.author},
-              &nbsp;
-              { new Intl.DateTimeFormat('en-Us',{
-                year:'numeric',
-                month:'long',
-                day:'2-digit'
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
 
-              }).format(new Date(comment.date))}
-             </p>
-             </li>
+    renderComments(dish) {
+        if (dish != null) {
+            const comments = dish.comments.map((comment) => {
+                return (
+                    <div key={comment.id}>
+                        <li>
+                            <p>{comment.comment}</p><br />
+                        </li>
+                        <li>
+                            <p>-- {comment.author}, {comment.date}</p>
+                        </li>
+                        </div>
+                );
+            });
+
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                    {comments}
+                    </ul>
+                </div>
             )
-          })
-          return (
-            <div className='col-12 col-md-5 m-1'>
-            <h4>comments</h4>
-            <ul className='list-unstyled'>
-                  {cmnts}
-            </ul>
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.dish)}
+                </div>
             </div>
-          )}
+        );
+    }
+}
 
-
-  renderDish(dish) {
-         if (dish != null) {
-             return (
-                 <div className='col-12 col-md-5 m-1'>
-                     <Card>
-                         <CardImg width="100%" src={dish.image} alt={dish.name} />
-                         <CardBody>
-                             <CardTitle>{dish.name}</CardTitle>
-                             <CardText>{dish.description}</CardText>
-                         </CardBody>
-                     </Card>
-                 </div>
-             )
-         }
-         else {
-             return (<div></div>)
-         }
-     }
-
-
-     render() {
-         const dish = this.props.dish
-         if (dish == null) {
-             return (<div></div>)
-         }
-         const dishItem = this.renderDish(dish)
-         const commentItem=this.renderComments(dish.comment)
-         return (
-             <div className='row'>
-                 {dishItem}
-                 {commentItem}
-
-             </div>
-         )
-     }
- }
-
-
-export default Dishdetail
+export default DishDetail;
