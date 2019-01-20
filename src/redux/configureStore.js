a0cfb321e299d588096b0d1879df1d7ab304d151
hidai25 <hidai25@gmail.com>
2019-01-20 16:51:26 +0200

    client-server communication

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,18 +5,23 @@ import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 
 
 
 export const configureStore = () => {
     const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders
-        }),
+      combineReducers({
+          dishes: Dishes,
+          comments: Comments,
+          promotions: Promotions,
+          leaders: Leaders,
+          ...createForms({
+              feedback: InitialFeedback
+          })
+      }),
           applyMiddleware(thunk, logger)
     );
 
